4317e9f406ad4a274a0d19c3411440dc4bd73f01
You Zhang <hydeout@YoudeMacBook-Pro.local>
2019-12-09 23:41:26 -0500

    add gitignore, fixed reading reset in js, working serial communication for multiple readings

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,52 +1,96 @@
+#include <Adafruit_NeoPixel.h>
+
+/* declare variables */
+
+// NeoPixel 
+#define PIN 5
+#define NUM_LEDS 1
+#define DELAYVAL 200
+//create a NeoPixel strip
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+// end NeoPixel
+
+// servo
 const int servoPin = 9;
-const int ledPin = 11;
+// end servo
 
-boolean haveLeftHand = false;
-float handRotateL = 0;
-float handHeightL = 0;
-float fingerDistL = 0;
+// data vairables
+//boolean haveLeftHand = false;
+//float handRotateL = 30;
+//float handHeightL = 60;
+//float fingerDistL = 90;
+//
+//boolean haveRightHand = false;
+//float handRotateR = 30;
+//float handHeightR = 60;
+//float fingerDistR = 90;
+// end data variables
 
-boolean haveRightHand = false;
-float handRotateR = 0;
-float handHeightR = 0;
-float fingerDistR = 0;
+
+int handHeightR = 0;
+int handRotateR = 0;
+String inData;
+String handHeightRString;
+
 
 // https://www.arduino.cc/en/Reference/StringLibrary
-String inData; // variable for reading from serial port - from p5.js 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  // initialize serial port
   Serial.begin(9600);
+
+  // start the strip and blank it out
+  strip.setBrightness(0);
+  strip.begin();
+
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(servoPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
 }
-
-
 
 // the loop function runs over and over again forever
 void loop() {
-  if (Serial.available() > 0) {   // see if there's incoming serial data
-    inData = Serial.read(); // read it
-    haveLeftHand = getValue(inData, ' ', 0);
-    handRotateL = getValue(inData, ' ', 1).toFloat();
-    handHeightL = getValue(inData, ' ', 2).toFloat();
-    fingerDistL = getValue(inData, ' ', 3).toFloat();
-    
-    haveRightHand = getValue(inData, ' ', 4);
-    handRotateR = getValue(inData, ' ', 5).toFloat();
-    handHeightR = getValue(inData, ' ', 6).toFloat();
-    fingerDistR = getValue(inData, ' ', 7).toFloat();
-  } else { }
-  Serial.print("handRotateL: ");
-  Serial.println(handRotateL);
-}
+  
+  // read the input from p5.js
+  if ( Serial.available()) { // when there is input
+    inData = Serial.readStringUntil('\n'); // read it until new line
+    int firstValueEnd = inData.indexOf(','); // find the first comma
+    handHeightRString = inData.substring(0, firstValueEnd); // first string
+    handHeightR = handHeightRString.toInt();
 
+    int secondValueEnd = inData.indexOf(',', firstValueEnd+1);
+    String handRotateRString = inData.substring(firstValueEnd+1, secondValueEnd);
+    handRotateR = handRotateRString.toInt();
+
+//    haveLeftHand = getValue(inData, ' ', 0);
+//    handRotateL = getValue(inData, ' ', 1).toFloat();
+//    handHeightL = getValue(inData, ' ', 2).toFloat();
+//    fingerDistL = getValue(inData, ' ', 3).toFloat();
+//    
+//    haveRightHand = getValue(inData, ' ', 4);
+//    handRotateR = getValue(inData, ' ', 5).toFloat();
+//    handHeightR = getValue(inData, ' ', 6).toFloat();
+//    fingerDistR = getValue(inData, ' ', 7).toFloat();
+  } 
+//
+  Serial.print("indata: ");
+  Serial.println(inData);
+//
+//  Serial.print("handHeightRString:");
+//  Serial.println(handHeightRString);
+//
+//  Serial.print("handHeightR:");
+//  Serial.println(handHeightR);
+
+  strip.setBrightness(handHeightR);
+  setAll(handRotateR,255,0);
+}
 
 
 // split string in C: https://stackoverflow.com/questions/9072320/split-string-into-string-array
 
- String getValue(String data, char separator, int index)
+String getValue(String data, char separator, int index)
 {
   int found = 0;
   int strIndex[] = {0, -1};
@@ -61,3 +105,41 @@ void loop() {
   }
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
+
+/* NeoPixel functions */
+
+/* end NeoPixel functions */
+
+
+/* helper functions for NeoPixel */
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue);
+  }
+  showStrip();
+}
+/* end helper */
--- js_code/sketch.js
+++ js_code/sketch.js
@@ -5,44 +5,49 @@
 // https://developer-archive.leapmotion.com/documentation/v2/javascript/api/Leap.Hand.html
 
 let serial;
+let portName = '/dev/tty.usbserial-14110';
+let inData;
+let outData;
+
+
 let latestData = "waiting for data";
 
-let handX = 100, handY = 100;
+let handX = 100,
+  handY = 100;
 
 
 
 /* ------------------- leap motion stuff ------------------- */
 
 let handNum;
-let haveLeftHand, leftHand, handRotateL, handHeightL,fingerDistL;
+let haveLeftHand, leftHand, handRotateL, handHeightL, fingerDistL;
 let haveRightHand, rightHand, handRotateR, handHeightR, fingerDistR;
 let currentActiveCube; // ++++++++++++++++++++++++ WIP ++++++++++++++++++++++++
 
 
-Leap.loop(function(frame) {
-  
+Leap.loop(function (frame) {
+
   handNum = frame.hands.length;
 
-  if(frame.hands.length > 0){
-    
+  if (frame.hands.length > 0) {
 
     let hand = frame.hands[0];
     handX = hand.stabilizedPalmPosition[0];
     handY = hand.stabilizedPalmPosition[1];
 
-
     // update hand stats
-    for (let i=0; i<frame.hands.length; i++){
-      if(frame.hands[i].type == 'left'){
-        console.log('have left hand');
+    for (let i = 0; i < frame.hands.length; i++) {
+      if (frame.hands[i].type == 'left') {
+        // console.log('have left hand');
         haveLeftHand = true;
         leftHand = frame.hands[i];
         fingerDistL = leftHand.sphereRadius;
         handRotateL = leftHand.palmNormal[1]; // -1: palm facing down; 1: palm facing up
         handHeightL = leftHand.stabilizedPalmPosition[1];
       }
-      if(frame.hands[i].type == 'right'){
-        console.log('have right hand');
+
+      if (frame.hands[i].type == 'right') {
+        // console.log('have right hand');
         haveRightHand = true;
         rightHand = frame.hands[i];
         fingerDistR = rightHand.sphereRadius;
@@ -50,30 +55,37 @@ Leap.loop(function(frame) {
         handHeightR = rightHand.stabilizedPalmPosition[1];
       }
     }
-    
-    if (haveLeftHand == true && haveRightHand == true){
-      console.log("have both hands");
+
+    if (haveLeftHand == true && haveRightHand == true) {
+      // console.log("have both hands");
     }
 
     // currentActiveCube = getActiveCube(frame.hands[0], minX, maxX, minY, maxY); // ++++++++++++++++++++++++ WIP ++++++++++++++++++++++++
-    
-  } else { console.log('no hands in the frame'); }
+
+  } else {
+    // console.log('no hands in the frame');
+    haveLeftHand = false;
+    haveRightHand = false;
+    fingerDistL = 0;
+    handRotateL = 0;
+    handHeightL = 0;
+    fingerDistR = 0;
+    handRotateR = 0;
+    handHeightR = 0;
+  }
 
   updateHTML(haveLeftHand, leftHand, fingerDistL, handRotateL, handHeightL, haveRightHand, rightHand, fingerDistR, handRotateR, handHeightR);
 
   // reset variables for the next frame
-  haveLeftHand = false; 
-  haveRightHand = false; 
+  // haveLeftHand = false;
+  // haveRightHand = false;
   currentActiveCube = null;
-  
-  
-    
 });
 
 function updateHTML(haveLeftHand, leftHand, fingerDistL, handRotateL, handHeightL,
-                    haveRightHand, rightHand, fingerDistR, handRotateR, handHeightR){
+  haveRightHand, rightHand, fingerDistR, handRotateR, handHeightR) {
   document.getElementById("haveLeftHand").innerHTML = haveLeftHand;
-  if (haveLeftHand == true){
+  if (haveLeftHand == true) {
     document.getElementById("fingerDistL").innerHTML = fingerDistL;
     document.getElementById("handRotateL").innerHTML = handRotateL;
     document.getElementById("handHeightL").innerHTML = handHeightL;
@@ -84,7 +96,7 @@ function updateHTML(haveLeftHand, leftHand, fingerDistL, handRotateL, handHeight
   }
 
   document.getElementById("haveRightHand").innerHTML = haveRightHand;
-  if (haveRightHand == true){
+  if (haveRightHand == true) {
     document.getElementById("fingerDistR").innerHTML = fingerDistR;
     document.getElementById("handRotateR").innerHTML = handRotateR;
     document.getElementById("handHeightR").innerHTML = handHeightR;
@@ -93,35 +105,46 @@ function updateHTML(haveLeftHand, leftHand, fingerDistL, handRotateL, handHeight
     document.getElementById("handRotateR").innerHTML = '-';
     document.getElementById("handHeightR").innerHTML = '-';
   }
-
 }
 
 function getActiveCube(hand, minX, maxX, minY, maxY) {
-  let x_1 = minX + (maxX - minX) * 1/3;
-  let x_2 = minX + (maxX - minX) * 2/3;
-  let y_1 = minY + (maxY - minY) * 1/3;
-  let y_2 = minY + (maxY - minY) * 2/3;
+  let x_1 = minX + (maxX - minX) * 1 / 3;
+  let x_2 = minX + (maxX - minX) * 2 / 3;
+  let y_1 = minY + (maxY - minY) * 1 / 3;
+  let y_2 = minY + (maxY - minY) * 2 / 3;
   let Xpos = hand.stabilizedPalmPosition[0];
   let Ypos = hand.stabilizedPalmPosition[1];
   let cubeIndex; // starting from 0
 
-  if (Ypos <= y_1){
+  if (Ypos <= y_1) {
 
-        if (Xpos <= x_1){ cubeIndex = 0; }
-        else if (Xpos > x_1 && Xpos < x_2){ cubeIndex = 1; }
-        else if (Xpos >= x_2){ cubeIndex = 2; }
+    if (Xpos <= x_1) {
+      cubeIndex = 0;
+    } else if (Xpos > x_1 && Xpos < x_2) {
+      cubeIndex = 1;
+    } else if (Xpos >= x_2) {
+      cubeIndex = 2;
+    }
 
-  } else if (Ypos > y_1 && Ypos < y_2){
+  } else if (Ypos > y_1 && Ypos < y_2) {
 
-        if (Xpos <= x_1){ cubeIndex = 3; }
-        else if (Xpos > x_1 && Xpos < x_2){ cubeIndex = 4; }
-        else if (Xpos >= x_2){ cubeIndex = 5; }
+    if (Xpos <= x_1) {
+      cubeIndex = 3;
+    } else if (Xpos > x_1 && Xpos < x_2) {
+      cubeIndex = 4;
+    } else if (Xpos >= x_2) {
+      cubeIndex = 5;
+    }
 
-  } else if (Ypos >= y_1){
+  } else if (Ypos >= y_1) {
 
-        if (Xpos <= x_1){ cubeIndex = 6; }
-        else if (Xpos > x_1 && Xpos < x_2){ cubeIndex = 7; }
-        else if (Xpos >= x_2){ cubeIndex = 8; }
+    if (Xpos <= x_1) {
+      cubeIndex = 6;
+    } else if (Xpos > x_1 && Xpos < x_2) {
+      cubeIndex = 7;
+    } else if (Xpos >= x_2) {
+      cubeIndex = 8;
+    }
 
   }
   return cubeIndex;
@@ -131,62 +154,71 @@ function getActiveCube(hand, minX, maxX, minY, maxY) {
 /* ------------------------ set up ------------------------ */
 
 function setup() {
- createCanvas(500, 500);
+  createCanvas(500, 500);
 
- serial = new p5.SerialPort();
+  //set up communication port
+  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial.on('list', printList); // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen); // callback for the port opening
+  serial.on('data', serialEvent); // callback for when new data arrives
+  serial.on('error', serialError); // callback for errors
+  serial.on('close', portClose); // callback for the port closing
 
- serial.list();
- serial.open('/dev/tty.usbserial-1440');
- serial.on('connected', serverConnected);
- serial.on('list', gotList);
- serial.on('data', gotData);
- serial.on('error', gotError);
- serial.on('open', gotOpen);
- serial.on('close', gotClose);
-  
+  serial.list(); // list the serial ports
+  serial.open(portName); // open a serial port
 }
 
-function serverConnected() {
- print("Connected to Server");
-}
+// Following functions print the serial communication status to the console for debugging purposes
 
-function gotList(thelist) {
- print("List of Serial Ports:");
-
- for (let i = 0; i < thelist.length; i++) {
-  print(i + " " + thelist[i]);
+function printList(portList) {
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+  // Display the list the console:
+  print(i + " " + portList[i]);
+  }
  }
-}
-
-function gotOpen() {
- print("Serial Port is Open");
-}
-
-function gotClose(){
- print("Serial Port is Closed");
- latestData = "Serial Port is Closed";
-}
-
-function gotError(theerror) {
- print(theerror);
-}
-
-function gotData() {
- let currentString = serial.readLine();
-  trim(currentString);
- if (!currentString) return;
- console.log(currentString);
- latestData = currentString;
-}
+ 
+ function serverConnected() {
+   print('connected to server.');
+ }
+ 
+ function portOpen() {
+   print('the serial port opened.')
+ }
+ 
+ function serialEvent() {
+  if(serial.available() > 0){
+    inData = Number(serial.readLine());
+  } else {
+    console.log("No Input");
+  }
+ }
+ 
+ function serialError(err) {
+   print('Something went wrong with the serial port. ' + err);
+ }
+ 
+ function portClose() {
+   print('The serial port closed.');
+ }
 
 
 /* ------------------------ draw ------------------------ */
 
-function draw(){
+function draw() {
   background(100);
   // console.log(handX);
-  
-  ellipse(map(round(handX), -width*0.25, width*0.25, 0, width), 50, 20, 20);
-  let outData =  haveLeftHand + " " + handRotateL + " " + handHeightL + " " + fingerDistL + " " + haveRightHand + " " + handRotateR + " " + handHeightR + " " + fingerDistR;
+
+  ellipse(map(round(handX), -width * 0.25, width * 0.25, 0, width), 50, 20, 20);
+
+  // let outData = haveLeftHand + "," + handRotateL + "," + handHeightL + "," + fingerDistL + "," + haveRightHand + "," + handRotateR + "," + handHeightR + "," + fingerDistR;
+
+  let outData = int(map(handHeightR, 0, 500, 0, 255)) + "," + int(map(handRotateR, -1, 1, 0, 255))+"\n";
+
+
+  console.log("outdata: " + outData);
+
   serial.write(outData);
+  serial.clear();
 }
